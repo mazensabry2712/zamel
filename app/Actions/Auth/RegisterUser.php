89bee1497d154dc6177b97ede16ad9c5dbe240ee
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-16 11:38:31 +0300

    Align RegisterUser with User/Profile separation

--- app/Actions/Auth/RegisterUser.php
+++ app/Actions/Auth/RegisterUser.php
@@ -7,28 +7,12 @@ use Illuminate\Support\Facades\Hash;
 
 class RegisterUser
 {
-
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
-    {
-        //
-    }
     public function execute(array $data): User
     {
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'university_id' => $data['university_id'],
-            'faculty_id' => $data['faculty_id'],
-            'academic_year_id' => $data['academic_year_id'],
-
-
-
         ]);
-
-       
     }
 }
