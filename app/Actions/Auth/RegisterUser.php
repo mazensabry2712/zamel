55913ff61dbd74d7a1380b379e8be9eae3916ea2
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-16 12:00:35 +0300

    Create user and initial profile during registration

--- app/Actions/Auth/RegisterUser.php
+++ app/Actions/Auth/RegisterUser.php
@@ -2,17 +2,31 @@
 
 namespace App\Actions\Auth;
 
+use App\Models\Profile;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterUser
 {
     public function execute(array $data): User
     {
-        return User::create([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
+        return DB::transaction(function () use ($data): User {
+            $user = User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'password' => Hash::make($data['password']),
+            ]);
+
+            $user->profile()->create([
+                'education_type' => $data['education_type'],
+                'university_id' => $data['university_id'] ?? null,
+                'faculty_id' => $data['faculty_id'] ?? null,
+                'school_id' => $data['school_id'] ?? null,
+                'academic_year_id' => $data['academic_year_id'] ?? null,
+            ]);
+
+            return $user;
+        });
     }
 }
