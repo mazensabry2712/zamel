57aa809d942243855980c0d3c79767898c574147
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-16 15:34:13 +0300

    Resolve FacultyResource merge conflict

--- app/Http/Resources/FacultyResource.php
+++ app/Http/Resources/FacultyResource.php
@@ -14,19 +14,11 @@ class FacultyResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-<<<<<<< HEAD
-        // return parent::toArray($request);
-=======
->>>>>>> 9b584c399be498740e4073dad7543e71aafd55f6
         return [
             'id' => $this->id,
             'university_id' => $this->university_id,
             'name' => $this->name,
-<<<<<<< HEAD
-            'slug' => $this->slug,        ];
-=======
             'slug' => $this->slug,
         ];
->>>>>>> 9b584c399be498740e4073dad7543e71aafd55f6
     }
 }
