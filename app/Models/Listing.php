74cd01d0b56f598eb1e8124a06d4193fe5a6214b
11maznsabry11 <111mazensabry111@gmail.com>
2026-08-18 00:20:22 +0300

    Implement HasMedia interface in Listing model for media management

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -5,9 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
 class Listing extends Model
+// class Listing extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory  HasMedia;
 
     protected $fillable = [
         'user_id',
