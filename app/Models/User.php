ec5a945073f0fcfc3f037f560981b59797fb0083
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-17 15:05:25 +0300

    Fix User mass assignment for moderation fields and role

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,14 +10,21 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable([
+    'name',
+    'email',
+    'password',
+    'status',
+    'role',
+    'suspended_until',
+    'moderation_reason',
+    'moderated_at',
+])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasApiTokens, HasFactory, Notifiable;
-
-  
 
     protected function casts(): array
     {
