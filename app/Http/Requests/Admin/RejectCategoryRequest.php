39df3a6748ab3e480c278047ab0f9368dd24e6a0
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-17 15:08:47 +0300

    Add category rejection request

---
+++ app/Http/Requests/Admin/RejectCategoryRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RejectCategoryRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'reason' => [
+                'required',
+                'string',
+                'min:5',
+                'max:1000',
+            ],
+        ];
+    }
+}
