94bd0999d975fb2ef93cf916aea5f44a8e7d0046
11maznsabry11 <111mazensabry111@gmail.com>
2026-08-17 23:08:30 +0300

    Add PublishListing action and corresponding test

---
+++ app/Actions/Listing/PublishListing.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Actions\Listing;
+
+class PublishListing
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
