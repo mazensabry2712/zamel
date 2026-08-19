31d05ee01cb1822a82653b6c0ae73cb2ea23b161
mazen sabry bahnes <128668286+mazensabry2712@users.noreply.github.com>
2026-08-19 21:16:56 +0300

    Add reservation confirmation endpoint

--- app/Http/Controllers/Api/V1/ReservationController.php
+++ app/Http/Controllers/Api/V1/ReservationController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Actions\Reservation\ConfirmReservation;
 use App\Actions\Reservation\CreateReservation;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ReservationResource;
 use App\Models\Listing;
+use App\Models\Reservation;
 use Illuminate\Http\JsonResponse;
 
 class ReservationController extends Controller
@@ -24,5 +26,19 @@ class ReservationController extends Controller
             'message' => 'Reservation created successfully.',
             'data' => new ReservationResource($reservation),
         ], 201);
+    }
+
+    public function confirm(
+        Listing $listing,
+        Reservation $reservation,
+        ConfirmReservation $confirmReservation,
+    ): ReservationResource {
+        $reservation = $confirmReservation->execute(
+            user: request()->user(),
+            listing: $listing,
+            reservation: $reservation,
+        );
+
+        return new ReservationResource($reservation);
     }
 }
